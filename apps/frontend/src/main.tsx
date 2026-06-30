a5245669103ea9bb6bbf691644ca4b97499a2bba
fordnox <fordnox@gmail.com>
2026-07-01 00:43:13 +0300

    Move toaster position to bottom-right

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <QueryClientProvider client={queryClient}>
           <BrowserRouter>
             <App />
-            <Toaster position="bottom-left" richColors />
+            <Toaster position="bottom-right" richColors />
           </BrowserRouter>
         </QueryClientProvider>
     </HelmetProvider>
